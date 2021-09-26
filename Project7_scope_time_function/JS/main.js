772bca801265df616a5e9c12fc8a1cb1fa24b510
PyLawrence <89617029+PyLawrence@users.noreply.github.com>
2021-09-26 13:04:16 -0400

    added timFunc(Time_Function())

--- Project7_scope_time_function/JS/main.js
+++ Project7_scope_time_function/JS/main.js
@@ -39,3 +39,21 @@ function checkUsername(dispEle, inputEle) {
         document.getElementById(dispEle).innerText = "Invalid username."
     }
 }
+
+
+function timeFunc(dispEle) {
+    var time = new Date().getHours()
+    var reply;
+
+    if (time < 12 == time > 0) {
+        reply = "It is morning!";
+    }
+    else if (time >= 12 == time < 18) {
+        reply = "It is afternoon!";
+    }
+    else {
+        reply = "It is evening time!";
+    }
+
+    document.getElementById(dispEle).innerText = reply;
+}
